9bda8190bcca398a87c62983850e09323a1b35bd
acj <gitlab@acjs.net>
2018-02-09 17:32:03 +0100

    Current website.

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+
+if(isset($_SERVER['SERVER_PROTOCOL'])) header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+header('Status: 404 Not Found');
+
+?>
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<meta property="fb:admins" content="509248955">
+<meta property="fb:app_id" content="449320888508627">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<title>404</title>
+<link rel="apple-touch-icon-precomposed" href="http://a00ade.com/apple-touch-icon-precomposed.png">
+<link rel="author" href="http://alexanderchristiaanjacob.com/">
+<link rel="shortcut icon" href="favicon.ico">
+<body bgcolor="alexander" text="alexander">
+<h1>404</h1>
